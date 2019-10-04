f9ea134bbd506645ee954a34ad611510c6bbc0ff
mbpup <mateusbp.up@gmail.com>
2019-10-04 00:51:44 -0300

    header mapping finished

---
+++ arg_parser.py
@@ -0,0 +1,50 @@
+import argparse
+
+parser = argparse.ArgumentParser(description="...")
+
+
+def parse_var(s):
+    """
+    Parse a key, value pair, separated by '='
+    That's the reverse of ShellArgs.
+
+    On the command line (argparse) a declaration will typically look like:
+        foo=hello
+    or
+        foo="hello world"
+    """
+    value = ""
+    items = s.split('=')
+    key = items[0].strip()  # we remove blanks around keys, as is logical
+    if len(items) > 1:
+        # rejoin the rest:
+        value = '='.join(items[1:])
+    return key, value
+
+
+def parse_vars(items):
+    """
+    Parse a series of key-value pairs and return a dictionary
+    """
+    d = {}
+
+    if items:
+        for item in items:
+            key, value = parse_var(item)
+            d[key] = value
+    return d
+
+
+def parse_argments():
+    parser.add_argument("--set",
+                        metavar="KEY=VALUE",
+                        nargs='+',
+                        help="Set a number of key-value pairs "
+                             "(do not put spaces before or after the = sign). "
+                             "If a value contains spaces, you should define "
+                             "it with double quotes: "
+                             'foo="this is a sentence". Note that '
+                             "values are always treated as strings.")
+    args = parser.parse_args()
+    values = parse_vars(args.set)
+    return values
---
+++ main.py
@@ -0,0 +1,210 @@
+import sys
+import os
+import json
+import csv
+import pandas as pd
+from arg_parser import parse_argments
+
+
+def create_metadata_file(path):
+    '''
+    Create new metadata file base on the user input.
+    User will input medatada fields in comma separated.
+    At last it will prompt to confirm user typed correctly,
+        if the user press 'Y' or 'y' key, typed medatada would be accepted.
+        if the user press any other keys, user can retype metadata header.
+    :param path: metadata directory path
+    :return: None
+    '''
+    print("Creating matadata file...")
+    headers = []
+    while True:
+        header_string = input("Please input headers separating comma:")
+        str = "["
+        headers = header_string.replace(" ", "").split(',')
+        if "" in headers:
+            print("Empty header found, try again please.\n")
+        else:
+            for header in headers:
+                if header != headers[-1]:
+                    str = str + header + ", "
+                else:
+                    str = str + header + "]"
+            answer = input("Would you save header{}?[Y]".format(str))
+            if answer == 'y' or answer == 'Y' or answer == "":
+                break
+
+    metadata = {}
+    for header in headers:
+        metadata[header] = [header, ]
+
+    with open(path, 'w') as metadata_file:
+        json.dump(metadata, metadata_file, indent=4)
+    print("metadata file created: {}".format(path))
+
+
+def mapping_header2metadata(directory_path, header):
+    '''
+    It will check the csv/xlsx header and map the header fields to the metadata.
+    :param directory_path: metadata file directory path
+    :param header: csv/xlsx header from a specific file
+    :return: dictionary data, key: csv header field, value: metadata field
+    '''
+    metadata = {}
+    metadata_updated = False
+    with open(directory_path + '/metadata.json') as medatada_file:
+        metadata = json.load(medatada_file)
+
+    header_mapping = {}
+    for field in header:
+        header_mapping[field] = ''
+        for meta_field, meta_alternatives in metadata.items():
+            if field in meta_alternatives:
+                header_mapping[field] = meta_field
+                break
+        if header_mapping[field] == '':
+            while True:
+                print("{}:New header found. Select a below mapping options.".format(field))
+                mapping_list = ["Ignore", ]
+                print_string = "[0]:Ignore\n"
+                count = 1
+                for meta_field in metadata:
+                    print_string += "[{}]: {}\n".format(count, meta_field)
+                    mapping_list.append(meta_field)
+                    count += 1
+                print_string += "[{}]: Add new metadata\n".format(count)
+                print(print_string)
+
+                while True:
+                    try:
+                        option = int(input("[0]"))
+                        break
+                    except ValueError:
+                        print("Invalid input. Try again.")
+                        continue
+
+                if option == 0:
+                    header_mapping[field] = 0
+                    break
+                elif option == count:
+                    header_mapping[field] = 0
+                    metadata[field] = [field, ]
+                    metadata_updated = True
+                    break
+                elif count > option > 0:
+                    header_mapping[field] = mapping_list[option]
+                    metadata[mapping_list[option]].append(field)
+                    metadata_updated = True
+                    break
+                else:
+                    print("Please input correct number[0~{}]".format(count))
+
+    if metadata_updated:
+        print(metadata)
+        with open(directory_path + '/metadata.json', 'w') as metadata_file:
+            json.dump(metadata, metadata_file, indent=4)
+
+    print(header_mapping)
+    return header_mapping
+
+def mapping_row2medatada(header_mapping, row):
+    mapped_data = {}
+
+    return mapped_data
+
+
+def parse_csv_file(directory_path, file):
+    path = '{}/{}'.format(directory_path, file)
+    print("parsing {}".format(path))
+
+    header_mapping = {}
+    mapped_data_list = []
+    with open(path, 'r') as csvFile:
+        reader = csv.reader(csvFile)
+        line_count = 0
+        for row in reader:
+            if line_count == 0:
+                header_mapping = mapping_header2metadata(directory_path, row)
+            else:
+                mapped_data = mapping_row2medatada(header_mapping, row)
+                mapped_data_list.append(mapped_data)
+            line_count += 1
+            print(row)
+    csvFile.close()
+
+    return mapped_data_list
+
+
+def parse_xlsx_file(directory_path, file):
+    path = '{}/{}'.format(directory_path, file)
+    print("parsing {}".format(path))
+    parsed = []
+    return parsed
+
+
+def join_files(directory_path, output_path):
+    print("Joining files from {}, to {}".format(directory_path, output_path))
+    files = os.listdir(directory_path)
+
+    joined_list = []
+    parsed_list = []
+    for file in files:
+        if file.split('.')[-1] == 'csv':
+            parsed_list = parse_csv_file(directory_path, file)
+        elif file.split('.')[-1] == 'xlsx':
+            parsed_list = parse_xlsx_file(directory_path, file)
+        else:
+            continue
+        joined_list.extend(parsed_list)
+
+
+def main():
+    print("***Checking commandline options***")
+    opt_values = parse_argments()
+
+    data_directory = ""
+    try:
+        data_directory = opt_values['dir'].strip("/")
+        if not os.path.isdir(data_directory):
+            print("Directory not exists: path={}".format(data_directory))
+            sys.exit()
+    except KeyError:
+        print("Directory option not found: dir=<path>")
+        sys.exit()
+    print("Start joining files from {}".format(data_directory))
+
+    output_file = ""
+    if 'output' not in opt_values:
+        output_file = "{}/{}.csv".format(data_directory, data_directory.split('/')[-1])
+        print("Output option not found. Result will saved in {}".format(output_file))
+    else:
+        output_file = opt_values['output']
+        filename = output_file.split("/")[-1]
+        output_directory = output_file.replace(filename, "")
+        if not os.path.isdir(output_directory):
+            print("Output directory {} not found.".format(output_directory))
+            sys.exit()
+        if filename.split('.')[-1] != 'csv':
+            print("Invalid output file name: {}".format(filename))
+            sys.exit()
+
+    print("\n***Checking matadata file***")
+    medatada_path = data_directory + "/metadata.json"
+    if not os.path.exists(medatada_path):
+        answer = input("matadata file not found. Create now?[Y]:")
+        if answer == 'y' or answer == 'Y' or answer == "":
+            create_metadata_file(medatada_path)
+        else:
+            metadata = {}
+            with open(medatada_path, 'w') as metadata_file:
+                json.dump(metadata, metadata_file, indent=4)
+            print("Skip adding metadata.")
+    else:
+        print("matadata file found: {}".format(medatada_path))
+
+    print("\n***Joining files ***")
+    join_files(data_directory, output_file)
+
+
+if __name__ == '__main__':
+    main()
